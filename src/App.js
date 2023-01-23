53d6c75f0c735ccbecb6bd74b27b443d1a65264f
Abhith-S <abhithshaji@gmail.com>
2023-01-23 23:30:57 +0530

    homes listed on site

--- src/App.js
+++ src/App.js
@@ -19,12 +19,58 @@ function App() {
   //to store states of accounts
   const [account, setAccount] = useState(null);
 
+  //to set provider
+  const [provider, setProvider] = useState(null);
+
+  //save escrow contract to state
+  const [escrow, setEscrow] = useState(null);
+
+  //save homes
+  const [homes , setHomes] = useState([]);
+
   //get the blockchain data
   const loadBlockchainData = async () => {
     //get the data from metamask
     //window.ethereum is injected by metamask to the browser
-    //ita allows us to access blockchain
+    //ita allows us to access blockchain, provider is hardhat
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    //set the provider here
+    setProvider(provider)
+
+    //get the network
+    const network = await provider.getNetwork()
+
+    //get the javascript version of realEstate contract
+    //the config file has the address of contract and the file is imported as 'config'
+    //network.chainId will return 31337 which is the same as the one in config file
+    //from that file the contract address is  taken
+    //"RealEstate " is the abi of realestate contract imported here
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider);
+
+    //get the total supply of homes
+    const totalSupply = await realEstate.totalSupply();
+
+    //store homes to an array
+    const homes = [];
+
+    //loop through the mapping which stores nft and take each home
+    for(let i = 1; i <= totalSupply; i++){
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+
+    setHomes(homes)
+
+    console.log(homes);
+
+    
+    //load the js version of escrow contrac
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider);
+    setEscrow(escrow)
+
   };
 
   //useEffect hook runs a fn everytime a render occurs
@@ -45,7 +91,10 @@ function App() {
         <hr />
 
         <div className="cards">
-          <div className="card">
+          {homes.map((home, index) => (
+
+          
+          <div className="card" key={index}>
             <div className="card__image">
               <img src="" alt="Home" />
             </div>
@@ -59,6 +108,7 @@ function App() {
               <p>1234 Elm street</p>
             </div>
           </div>
+        ))}
         </div>
       </div>
     </div>
